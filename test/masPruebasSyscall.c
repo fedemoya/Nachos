6719fe18a87cd8de64b36d88215d628b1afdd031
Federico Moya <federicoamoya@gmail.com>
2012-06-23 11:09:37 -0300

    agrego mas pruebas

---
+++ test/masPruebasSyscall.c
@@ -0,0 +1,30 @@
+#include "syscall.h"
+
+int main()
+{
+	char algoParaEscribir[100];
+	char buffer[100];
+	int id0, id1, id2;
+	const int size = 12;
+
+	id0 = Open("./test/recursos/readme");
+	Read(buffer, size, id0);
+
+	Create("./test/recursos/writemeAAA");
+	id1 = Open("./test/recursos/writemeAAA");
+	Write(buffer, size, id1);
+
+	Create("./test/recursos/archivo");
+	id2 = (int) Open("./test/recursos/archivo");
+	algoParaEscribir[0] = 'D';
+	algoParaEscribir[1] = 'E';
+	algoParaEscribir[2] = 'F';
+	Write(algoParaEscribir, 3, id2);
+
+	Close(id0);
+	Close(id1);
+	Close(id2);
+
+    Exit(2);
+}
+
